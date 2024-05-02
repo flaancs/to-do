9c2515d4c6f6693f0987728a852fd6ff2a8b85f7
Gustavo Antonio <gustavo1998antonio@gmail.com>
2024-05-02 12:54:37 -0300

    feat: add shadcn components library

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,14 @@
-import { Inter } from "next/font/google";
+import { Inter as FontSans } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 import type { Metadata } from "next";
 
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
-const inter = Inter({ subsets: ["latin"] });
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 export const metadata: Metadata = {
   title: "To-do App",
@@ -18,8 +22,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable,
+        )}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
           {children}
         </ThemeProvider>
       </body>
