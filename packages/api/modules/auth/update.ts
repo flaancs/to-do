a9cc5909ab9e3ce4266c3afb21cf1ea8b7f3af6a
Gustavo Antonio <gustavo1998antonio@gmail.com>
2024-05-02 18:51:24 -0300

    feat: new endpoint to update user

---
+++ packages/api/modules/auth/update.ts
@@ -0,0 +1,66 @@
+import { TRPCError } from "@trpc/server";
+import { hashPassword, lucia, verifyPassword } from "@packages/auth";
+import { UserSchema, db } from "@packages/db";
+import { z } from "zod";
+import { protectedProcedure, publicProcedure } from "../../trpc/base";
+
+export const login = protectedProcedure
+  .input(
+    z.object({
+      id: z.string(),
+      name: z.string().optional(),
+      password: z.string().optional(),
+      passwordConfirmation: z.string().optional(),
+    }),
+  )
+  .output(
+    UserSchema.pick({
+      id: true,
+      email: true,
+      name: true,
+      avatarUrl: true,
+    }).partial({
+      avatarUrl: true,
+    }),
+  )
+  .mutation(
+    async ({
+      input: { id, name, password, passwordConfirmation },
+      ctx: { user },
+    }) => {
+      try {
+        if (id !== user.id) {
+          throw new TRPCError({
+            code: "FORBIDDEN",
+            message: "You are not authorized to perform this action.",
+          });
+        }
+
+        let newPassword = password;
+        if (newPassword && newPassword !== passwordConfirmation) {
+          throw new TRPCError({
+            code: "BAD_REQUEST",
+            message: "Passwords do not match.",
+          });
+        }
+
+        const updatedUser = await db.user.update({
+          where: {
+            id,
+          },
+          data: {
+            name,
+            ...(newPassword && { password: await hashPassword(newPassword) }),
+          },
+        });
+
+        return updatedUser;
+      } catch (error: any) {
+        console.error(error);
+        throw new TRPCError({
+          code: error.code || "INTERNAL_SERVER_ERROR",
+          message: error.message || "An unknown error occurred.",
+        });
+      }
+    },
+  );
