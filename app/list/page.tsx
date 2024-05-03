b911b8f1a3a04fc6beee3b10d92f560ac4651699
Gustavo Antonio <gustavo1998antonio@gmail.com>
2024-05-03 18:48:52 -0300

    chore: remove mocked data and add tasks query

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -1,81 +1,21 @@
-import { Button } from "@/components/ui/button";
-import { Task } from "@/components/ui/task";
-import { TaskPopover } from "@/components/ui/task-popover";
-import { PlusIcon } from "lucide-react";
-import moment from "moment";
-
-const tasks = [
-  {
-    id: "1",
-    title: "Buy groceries",
-    dueDate: moment().add(1, "day").toDate(),
-    color: "blue",
-    completed: false,
-    userId: "1",
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  },
-  {
-    id: "2",
-    title: "Walk the dog",
-    dueDate: moment().add(10, "day").toDate(),
-    color: "green",
-    completed: true,
-    userId: "1",
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  },
-  {
-    id: "3",
-    title: "Do laundry",
-    dueDate: moment().subtract(2, "day").toDate(),
-    color: "red",
-    completed: false,
-    userId: "1",
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  },
-  {
-    id: "4",
-    title: "Buy new laundry",
-    dueDate: moment().toDate(),
-    color: "red",
-    completed: false,
-    userId: "1",
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  },
-  {
-    id: "5",
-    title: "Do dishes",
-    color: "gray",
-    dueDate: null,
-    completed: true,
-    userId: "1",
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  },
-];
+"use client";
+import { Tasks } from "@/components/task/tasks";
+import { apiClient } from "@lib/api-client";
 
 export default function List() {
+  const {
+    data: tasks,
+    refetch: refetchTasks,
+    isLoading: isLoadingTasks,
+  } = apiClient.tasks.get.useQuery();
+
   return (
     <div className="mx-auto w-full max-w-sm space-y-6">
-      <div className="mb-4 flex items-center justify-between">
-        <h2 className="text-2xl font-bold">Todo List</h2>
-        <TaskPopover>
-          <Button size="sm" variant="outline">
-            <PlusIcon className="mr-2 h-4 w-4" />
-            Add Task
-          </Button>
-        </TaskPopover>
-      </div>
-      <div className="w-full space-y-2">
-        {tasks?.length > 0 ? (
-          tasks.map((task) => <Task task={task} key={task.id} />)
-        ) : (
-          <p>No tasks found</p>
-        )}
-      </div>
+      <Tasks
+        tasks={tasks}
+        onRefreshTasks={refetchTasks}
+        isLoadingTasks={isLoadingTasks}
+      />
     </div>
   );
 }
